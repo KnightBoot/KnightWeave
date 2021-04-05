c782993c422b1d4fc0fe5b2b44a4e080dd23ad3c
zhuoxiuwu <zhuoxiuwu@hellobike.com>
2021-04-05 23:54:02 +0800

    apply transformer

--- weaver-plugin/src/main/java/com/knightboot/weaverplugin/WeavePlugin.java
+++ weaver-plugin/src/main/java/com/knightboot/weaverplugin/WeavePlugin.java
@@ -1,7 +1,11 @@
 package com.knightboot.weaverplugin;
 
+import com.android.build.gradle.BaseExtension;
+import com.knightboot.weaverplugin.transform.WeaveTransform;
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.ProjectConfigurationException;
 
 /**
  * Created by Knight-ZXW on 21/3/22.
@@ -9,6 +13,13 @@ import org.gradle.api.Project;
 public class WeavePlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
+        if (project.getPlugins().findPlugin("com.android.application") == null
+                && project.getPlugins().findPlugin("com.android.library") == null) {
+            throw new ProjectConfigurationException(project.getName() + " is not an android application/library", (Throwable) null);
+        }
+        BaseExtension baseExtension = (BaseExtension) project.getExtensions().getByName("android");
 
+        WeaveExtension weaveExtension = project.getExtensions().getByType(WeaveExtension.class);
+        baseExtension.registerTransform(new WeaveTransform(project,weaveExtension));
     }
 }
